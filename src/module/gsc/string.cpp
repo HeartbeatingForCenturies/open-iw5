4371a9843b031cd45b0e48996403557cd362d9ec
FutureRave <edoardo.sanguineti222@gmail.com>
2023-03-06 13:29:18 +0000

    feat: add GetChar utils function

---
+++ src/module/gsc/string.cpp
@@ -0,0 +1,37 @@
+#include <std_include.hpp>
+#include <loader/module_loader.hpp>
+#include "game/game.hpp"
+
+#include "script_error.hpp"
+#include "script_extension.hpp"
+
+namespace gsc
+{
+	class string final : public module
+	{
+	public:
+		void post_load() override
+		{
+			register_function("getchar", []
+			{
+				const auto* str = game::native::Scr_GetString(0);
+				const auto index = scr_get_int(1);
+
+				if (!str)
+				{
+					scr_error("^GetChar: Illegal parameter!\n");
+					return;
+				}
+
+				if (static_cast<std::size_t>(index) >= std::strlen(str))
+				{
+					scr_error("GetChar: char index is out of bounds");
+				}
+
+				game::native::Scr_AddInt(str[index]);
+			});
+		}
+	};
+}
+
+REGISTER_MODULE(gsc::string)
